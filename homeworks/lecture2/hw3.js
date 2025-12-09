a7602128dc276e94d158507384c07bf200fcef76
De Zeng <56708184+facedoodle@users.noreply.github.com>
2025-12-08 16:04:33 -0800

    Homeworks-shanshanMa

--- homeworks/lecture2/hw3.js
+++ homeworks/lecture2/hw3.js
@@ -2,29 +2,63 @@
 // Explain your answer.
 
 console.log(0.1 + 0.2);
+// 0.30000000000000004
+// both 0.1 and 0.2 here are number, and JS uses floating-point math, so some decimals can't be represented exactly
+// When added, some small precision errors happen, resulting in a number slightly greater than 0.3
 
 console.log(0.1 + 0.2 == 0.3);
+// false
+// becasue of the floating-point math, 0.1+0.2 we get 0.30000000000000004 should be slightly greater than 0.3, not exactly 0.3.
 
 console.log(1 +  "2" + "2");
+// "122"
+// "2" here are not a number but a string, so we concatenate instead of doing the calculation.
 
 console.log(1 +  +"2" + "2");
+// "32"
+// +"2" converted string "2" into number 2, so 1 + 2 = 3 and then concatenate to string "2", so return "32"
 
 console.log(1 +  -"1" + "2");
+// "02"
+// -"1" converts "1" into number -1, 1-1=0, concatenate to string "2", return "02".
 
 console.log(+"1" +  "1" + "2");
+// "112"
+// +"1" converts "1" into 1, concatenate to string "1" and "2", return "112"
 
 console.log( "A" - "B" + "2");
+// "NaN2"
+// both "A" and "B" are not a number, we cannot subtract string, return NaN.
+// NaN is a string, concatenate to string "2", return "NaN2"
 
 console.log( "A" - "B" + 2);
+// NaN
+// Then same reason as above, return NaN when doing "A" - "B"
+// NaN + 2 = NaN
 
 console.log("0 || 1 = "+(0 || 1));
+// "0 || 1 = 1"
+// 0 is false, 1 is true, 0 || 1 return first true which is 1
+// concatenate string return "0 || 1 = 1"
 
 console.log("1 || 2 = "+(1 || 2));
+// "1 || 2 = 1"
+// 1 and 2 are both true, so 1 || 2 returns 1
+// concatenate string resturns "1 || 2 = 1"
 
 console.log("0 && 1 = "+(0 && 1));
+// "0 && 1 = 0"
+// && resturns first false value, 0 is false, so return 0,
+// concatenate strings resturn "0 && 1 = 0"
 
 console.log("1 && 2 = "+(1 && 2));
+// "1 && 2 = 2"
+// if both true, return the last true value, so return 2
 
 console.log(false == '0')
+// true
+// == does not compare the data type, false = 0, 0 == "0"
 
 console.log(false === '0')
+// false
+// === compares both data value and data type, 0 != "0"
--- homeworks/lecture2/hw6.js
+++ homeworks/lecture2/hw6.js
@@ -2,15 +2,34 @@
 
 // 1. Write a function that returns the largest element in a list.
 function largestElement(list) {
-    // implement your code here
+    let max = list[0]
+    for (let i = 1; i <list.length; i++){
+        if (list[i] > max){
+            max = list[i]
+        }
+    }
+    return max
 }
 
 // 2. Write function that reverses a list, preferably in place.
 function reverseList(list) {
-    // implement your code here
+    for (let i = 0; i < list.length / 2; i++){
+        let temp = list[i];
+        list[i] = list[length - 1 - i]
+        list[length -1 - i] = temp
+    }
 }
 
 // 3. Write a function that checks whether an element occurs at least twice in a list.
 function checkTwice(list, element) {
-    // implement your code here
+    let count = 0;
+    for (let item of list){
+        if (item === element){
+            count++;
+            if (count >= 2){
+                return true;
+            }
+        }
+    }
+    return false;
 }
