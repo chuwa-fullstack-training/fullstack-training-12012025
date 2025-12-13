d547324788f5619b2bfe3a596a7e0cbad164b908
DanXuuuuu <139434572+DanXuuuuu@users.noreply.github.com>
2025-12-13 14:00:06 -0800

    lecture4 hw2 and hw3

--- homeworks/lecture4/hw2.js
+++ homeworks/lecture4/hw2.js
@@ -1,10 +1,23 @@
 // ONLY use map, filter, reduce to solve the following problems:
 
 // 1. Given an array of numbers, return an array of numbers that are doubled.
-
+ function timesTwo(nums){
+    const doubled = nums.map(n=>n*2);
+    return doubled;
+ }
 // 2. Given an array of numbers, return an array of numbers that are even.
-
+function evenfilter(nums){
+    const even = nums.filter(n=>n%2===0);
+    return even;
+}
 // 3. Reverse the string: "Hello World" -> "dlroW olleH"
+function reverse(nums){
+    const reverseS = nums.split('').reduce((acc, num)=> {
+        return num+acc;
+    },'');
+    return reverseS;
+}
+console.log(reverse("Hello World"));
 
 /**
  * 4. Flatten the array of arrays to a single array:
@@ -15,3 +28,12 @@
  * const arr = [[0, 1], [2, 3], [4, [5, 6]]];
  * Expected output: [0, 1, 2, 3, 4, 5, 6]
  */
+
+// it's desctucturing? 
+function getSingle(arr){
+    const single = arr.reduce((acc, item)=>{
+        return acc.concat(Array.isArray(item)?getSingle(item): item)
+    }, [])
+  return single;
+}
+console.log(getSingle([[0, 1], [2, 3], [4, [5, 6]]]));
--- homeworks/lecture4/hw3.js
+++ homeworks/lecture4/hw3.js
@@ -6,5 +6,34 @@
  * const instance2 = new Singleton();
  * console.log(instance1 === instance2); // Output: true
  */
+// ES5 closure 
+function Singleton(){
+    if(Singleton.instance){
+        return Singleton.instance;
+    }
+    this.name = "singleton";
+    this.value = Math.random();
+    Singleton.instance = this;
+}
+const instance1 = new Singleton();
+const instance2 = new Singleton();
+console.log(instance1 === instance2); // Output: true
+console.log(instance1.value === instance2.value);  
 
-// your code here
+// ES6 class
+
+class Singleton{
+    constructor(){
+        if(Singleton.instance){
+        return Singleton.instance;
+    }
+    this.name = "singleton";
+    this.value = Math.random();
+    Singleton.instance = this;
+    }
+}
+
+const instance3 = new Singleton();
+const instance4 = new Singleton();
+console.log(instance3 === instance4); // Output: true
+console.log(instance3.value === instance4.value);  
